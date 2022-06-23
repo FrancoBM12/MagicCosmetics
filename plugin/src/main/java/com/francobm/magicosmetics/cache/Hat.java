85091e426139c3e6375d8b4ab679a60f44f40064
FrancoBM <baldeonjean@gmail.com>
2022-06-23 15:40:00 -0500

    feat: Changes and improvements in hats, sound problems in 1.16 and hats via packets implemented and fixed.

--- plugin/src/main/java/com/francobm/magicosmetics/cache/Hat.java
+++ plugin/src/main/java/com/francobm/magicosmetics/cache/Hat.java
@@ -1,22 +1,19 @@
 package com.francobm.magicosmetics.cache;
 
 import com.francobm.magicosmetics.MagicCosmetics;
+import com.francobm.magicosmetics.api.CosmeticType;
 import com.francobm.magicosmetics.nms.NPC.ItemSlot;
 import com.francobm.magicosmetics.utils.XMaterial;
 import org.bukkit.Color;
-import org.bukkit.Sound;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 
 public class Hat extends Cosmetic{
 
     private final boolean overlaps;
 
-    public Hat(String id, String name, ItemStack itemStack, int modelData, boolean colored, CosmeticType cosmeticType, Color color, boolean overlaps) {
-        super(id, name, itemStack, modelData, colored, cosmeticType, color);
+    public Hat(String id, String name, ItemStack itemStack, int modelData, boolean colored, CosmeticType cosmeticType, Color color, boolean overlaps, String permission, boolean texture, boolean hideMenu) {
+        super(id, name, itemStack, modelData, colored, cosmeticType, color, permission, texture, hideMenu);
         this.overlaps = overlaps;
     }
 
@@ -24,10 +21,10 @@ public class Hat extends Cosmetic{
     public void active(Player player) {
         ItemStack itemStack = player.getInventory().getHelmet();
         if(overlaps){
-            if(itemStack == null || itemStack.getType() == XMaterial.AIR.parseMaterial()){
+            /*if(itemStack == null || itemStack.getType() == XMaterial.AIR.parseMaterial()){
                 player.getInventory().setHelmet(getItemColor(player));
-            }
-            MagicCosmetics.getInstance().getVersion().equip(player, ItemSlot.HELMET, getItemColor(player), true);
+            }*/
+            MagicCosmetics.getInstance().getVersion().equip(player, ItemSlot.HELMET, getItemColor(player));
             return;
         }
         if(itemStack != null){
@@ -41,7 +38,12 @@ public class Hat extends Cosmetic{
     public void clear(Player player) {
         ItemStack itemStack = player.getInventory().getHelmet();
         if(overlaps){
-            MagicCosmetics.getInstance().getVersion().equip(player, ItemSlot.HELMET, itemStack, true);
+            MagicCosmetics.getInstance().getVersion().equip(player, ItemSlot.HELMET, itemStack);
+            /*MagicCosmetics.getInstance().getVersion().equip(player, ItemSlot.HELMET, XMaterial.AIR.parseItem());
+            if(itemStack != null && itemStack.getType() == getItemColor().getType()){
+                player.getInventory().setHelmet(null);
+                return;
+            }*/
             return;
         }
         if(itemStack == null){
