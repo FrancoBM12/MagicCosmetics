8744f4570c561db1512a7d1b94cac5e54f83520f
FrancoBM <baldeonjean@gmail.com>
2022-06-23 16:16:17 -0500

    feat: Improved api and new methods.

--- plugin/src/main/java/com/francobm/magicosmetics/api/MagicAPI.java
+++ plugin/src/main/java/com/francobm/magicosmetics/api/MagicAPI.java
@@ -1,19 +1,38 @@
 package com.francobm.magicosmetics.api;
 
 import com.francobm.magicosmetics.MagicCosmetics;
-import com.francobm.magicosmetics.cache.Cosmetic;
-import com.francobm.magicosmetics.cache.CosmeticType;
-import com.francobm.magicosmetics.cache.PlayerCache;
+import com.francobm.magicosmetics.cache.*;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class MagicAPI {
 
     public static boolean hasCosmetic(Player player, String cosmeticId){
+        MagicCosmetics plugin = MagicCosmetics.getInstance();
         PlayerCache playerCache = PlayerCache.getPlayer(player);
+        if(plugin.isPermissions()){
+            return Cosmetic.getCosmetic(cosmeticId).hasPermission(player);
+        }
         Cosmetic cosmetic = playerCache.getCosmeticById(cosmeticId);
         return cosmetic != null;
+    }
+
+    public static boolean spray(Player player) {
+        PlayerCache playerCache = PlayerCache.getPlayer(player);
+        if(playerCache.getSpray() == null) return false;
+        playerCache.draw(SprayKeys.API);
+        return true;
+    }
+
+    public static boolean tintItem(ItemStack item, String colorHex){
+        return MagicCosmetics.getInstance().getCosmeticsManager().tintItem(item, colorHex);
+    }
+
+    public static boolean hasEquipCosmetic(Entity entity, String cosmeticId){
+        EntityCache entityCache = EntityCache.getOrCreateEntity(entity.getUniqueId());
+        return entityCache.hasEquipped(cosmeticId);
     }
 
     public static boolean hasEquipCosmetic(Player player, CosmeticType cosmeticType){
@@ -22,14 +41,30 @@ public class MagicAPI {
         return cosmetic != null;
     }
 
-    public static void EquipCosmetic(Player player, String cosmeticId){
+    public static void EquipCosmetic(Player player, String cosmeticId, String color, boolean force){
         MagicCosmetics plugin = MagicCosmetics.getInstance();
-        plugin.getCosmeticsManager().useCosmetic(player, cosmeticId);
+        plugin.getCosmeticsManager().equipCosmetic(player, cosmeticId, color, force);
+    }
+
+    public static void EquipCosmetic(Entity entity, String cosmeticId, String colorHex){
+        EntityCache entityCache = EntityCache.getOrCreateEntity(entity.getUniqueId());
+        Cosmetic cosmetic = Cosmetic.getCloneCosmetic(cosmeticId);
+        if(cosmetic == null) return;
+        if(colorHex != null){
+            org.bukkit.Color color = Color.hex2Rgb(colorHex);
+            cosmetic.setColor(color);
+        }
+        entityCache.setCosmetic(cosmetic);
     }
 
     public static void UnEquipCosmetic(Player player, CosmeticType cosmeticType){
         MagicCosmetics plugin = MagicCosmetics.getInstance();
         plugin.getCosmeticsManager().unSetCosmetic(player, cosmeticType);
+    }
+
+    public static void UnEquipCosmetic(Entity entity, CosmeticType cosmeticType){
+        EntityCache entityCache = EntityCache.getOrCreateEntity(entity.getUniqueId());
+        entityCache.unSetCosmetic(cosmeticType);
     }
 
     public static ItemStack getCosmeticItem(String id){
@@ -46,9 +81,9 @@ public class MagicAPI {
             CosmeticType cosmeticType = CosmeticType.valueOf(type.toUpperCase());
             Cosmetic cosmetic = playerCache.getEquip(cosmeticType);
             if(cosmetic == null) return null;
-            return cosmetic.getItemColor();
+            return cosmetic.getItemColor().clone();
         }catch (IllegalArgumentException ignored){
-
+            MagicCosmetics.getInstance().getLogger().warning("Invalid cosmetic type: " + type);
         }
         return null;
     }
@@ -59,7 +94,11 @@ public class MagicAPI {
     }
 
     public static int getPlayerAllCosmeticsAvailable(Player player){
+        MagicCosmetics plugin = MagicCosmetics.getInstance();
         PlayerCache playerCache = PlayerCache.getPlayer(player);
+        if(plugin.isPermissions()){
+            return playerCache.getCosmeticsPerm().size();
+        }
         return playerCache.getCosmetics().size();
     }
 
