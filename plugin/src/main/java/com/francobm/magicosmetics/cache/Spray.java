e9f85b713f1b4a95a0e0c6d525c78c2e1e0a8596
FrancoBM <baldeonjean@gmail.com>
2022-06-23 16:17:38 -0500

    feat: New Cosmetic added.

---
+++ plugin/src/main/java/com/francobm/magicosmetics/cache/Spray.java
@@ -0,0 +1,207 @@
+package com.francobm.magicosmetics.cache;
+
+import com.francobm.magicosmetics.MagicCosmetics;
+import com.francobm.magicosmetics.api.CosmeticType;
+import com.francobm.magicosmetics.api.SprayKeys;
+import com.francobm.magicosmetics.cache.renderer.ImageRenderer;
+import com.francobm.magicosmetics.events.SprayDrawingEvent;
+import com.francobm.magicosmetics.nms.spray.CustomSpray;
+import com.francobm.magicosmetics.utils.MathUtils;
+import com.francobm.magicosmetics.utils.Utils;
+import com.francobm.magicosmetics.utils.XMaterial;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.RayTraceResult;
+
+import java.awt.image.BufferedImage;
+
+public class Spray extends Cosmetic {
+
+    private CustomSpray customSpray;
+    private BukkitTask bukkitTask;
+    private final BufferedImage image;
+    private final boolean itemImage;
+    private boolean paint = false;
+    private long coolDown;
+
+    public Spray(String id, String name, ItemStack itemStack, int modelData, boolean colored, CosmeticType cosmeticType, Color color, String permission, boolean texture, BufferedImage image, boolean itemImage, boolean hideMenu) {
+        super(id, name, itemStack, modelData, colored, cosmeticType, color, permission, texture, hideMenu);
+        this.itemImage = itemImage;
+        if(image == null) {
+            this.image = null;
+            return;
+        }
+        this.image = Utils.deepCopy(image);
+    }
+
+    public void draw(Player player, BlockFace blockFace, Location location, int rotation) {
+        MagicCosmetics plugin = MagicCosmetics.getInstance();
+        clear(player);
+        if(itemImage) {
+            ItemStack item = getItemColor(player);
+            ItemMeta itemMeta = item.getItemMeta();
+            itemMeta.setDisplayName("");
+            item.setItemMeta(itemMeta);
+            Utils.sendSound(player, Sound.getSound("spray"));
+            customSpray = plugin.getVersion().createCustomSpray(player, location, blockFace, item, null, rotation);
+            customSpray.spawn(player);
+            customSpray.setPreview(true);
+            bukkitTask = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if(customSpray == null) {
+                        cancel();
+                        return;
+                    }
+                    clear(player);
+                }
+            }.runTaskLater(plugin, plugin.getSprayStayTime());
+            return;
+        }
+
+        if(image != null) {
+            ItemStack map = Utils.getMapImage(player, image, this);
+            MapView mapView = ((MapMeta) map.getItemMeta()).getMapView();
+            Utils.sendSound(player, Sound.getSound("spray"));
+            customSpray = plugin.getVersion().createCustomSpray(player, location, blockFace, map.clone(), mapView, rotation);
+            customSpray.spawn(player);
+            customSpray.setPreview(true);
+        }
+
+        bukkitTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(customSpray == null) {
+                    cancel();
+                    return;
+                }
+                clear(player);
+            }
+        }.runTaskLater(plugin, plugin.getSprayStayTime());
+    }
+
+    public void draw(Player player, SprayKeys key){
+        MagicCosmetics plugin = MagicCosmetics.getInstance();
+        if(plugin.getSprayCooldown() > 0) {
+            if (coolDown > System.currentTimeMillis()) {
+                int seconds = (int) ((coolDown - System.currentTimeMillis()) / 1000);
+                plugin.getCosmeticsManager().sendMessage(player, plugin.prefix + plugin.getMessages().getString("spray-cooldown").replace("%time%", Utils.getTime(seconds)));
+                return;
+            }
+            long milliseconds = plugin.getSprayCooldown() * 1000L;
+            coolDown = System.currentTimeMillis() + milliseconds;
+        }
+        clear(player);
+        if(itemImage) {
+            ItemStack item = getItemColor(player);
+            ItemMeta itemMeta = item.getItemMeta();
+            itemMeta.setDisplayName("");
+            item.setItemMeta(itemMeta);
+            Location location = player.getEyeLocation();
+            RayTraceResult result = location.getWorld().rayTrace(location, location.getDirection(), 10, FluidCollisionMode.ALWAYS, false, 1, (entity) -> false);
+            if(result == null) return;
+            if(result.getHitEntity() != null && result.getHitEntity().getType() == EntityType.ITEM_FRAME) return;
+            final int rotation;
+            if(result.getHitBlockFace() == BlockFace.UP || result.getHitBlockFace() == BlockFace.DOWN) {
+                rotation = Utils.getRotation(player.getLocation().getYaw(), false) * 45;
+            } else {
+                rotation = 0;
+            }
+            SprayDrawingEvent event = new SprayDrawingEvent(player, result.getHitBlock(), key);
+            Bukkit.getPluginManager().callEvent(event);
+            if(event.isCancelled()) return;
+            Location frameLoc = result.getHitBlock().getRelative(result.getHitBlockFace()).getLocation();
+            Utils.sendAllSound(frameLoc, Sound.getSound("spray"));
+            customSpray = plugin.getVersion().createCustomSpray(player, frameLoc, result.getHitBlockFace(), item, null, rotation);
+            bukkitTask = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if(customSpray == null) {
+                        cancel();
+                        return;
+                    }
+                    clear(player);
+                }
+            }.runTaskLater(plugin, plugin.getSprayStayTime());
+            return;
+        }
+        if(image != null) {
+            ItemStack map = Utils.getMapImage(player, image, this);
+            MapView mapView = ((MapMeta) map.getItemMeta()).getMapView();
+            Location location = player.getEyeLocation();
+            RayTraceResult result = location.getWorld().rayTrace(location, location.getDirection(), 10, FluidCollisionMode.ALWAYS, false, 1, (entity) -> false);
+            if(result == null) return;
+            if(result.getHitEntity() != null && result.getHitEntity().getType() == EntityType.ITEM_FRAME) return;
+            final int rotation;
+            if(result.getHitBlockFace() == BlockFace.UP || result.getHitBlockFace() == BlockFace.DOWN) {
+                rotation = Utils.getRotation(player.getLocation().getYaw(), false) * 45;
+            } else {
+                rotation = 0;
+            }
+            SprayDrawingEvent event = new SprayDrawingEvent(player, result.getHitBlock(), key);
+            Bukkit.getPluginManager().callEvent(event);
+            if(event.isCancelled()) return;
+            Location frameLoc = result.getHitBlock().getRelative(result.getHitBlockFace()).getLocation();
+            Utils.sendAllSound(frameLoc, Sound.getSound("spray"));
+            customSpray = plugin.getVersion().createCustomSpray(player, frameLoc, result.getHitBlockFace(), map.clone(), mapView, rotation);
+        }
+
+        bukkitTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(customSpray == null) {
+                    cancel();
+                    return;
+                }
+                clear(player);
+            }
+        }.runTaskLater(plugin, plugin.getSprayStayTime());
+    }
+
+    @Override
+    public void active(Player player) {
+        if(customSpray == null) return;
+        if(customSpray.isPreview()) return;
+        customSpray.spawn(false);
+    }
+
+    @Override
+    public void clear(Player player) {
+        if(customSpray != null) {
+            customSpray.setPreview(false);
+            customSpray.remove();
+            customSpray = null;
+        }
+        if(bukkitTask != null) {
+            bukkitTask.cancel();
+            bukkitTask = null;
+        }
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public boolean isPaint() {
+        return paint;
+    }
+
+    public void setPaint(boolean paint) {
+        this.paint = paint;
+    }
+
+    public boolean isItemImage() {
+        return itemImage;
+    }
+}
---
+++ plugin/src/main/java/com/francobm/magicosmetics/cache/inventories/menus/SprayMenu.java
@@ -0,0 +1,166 @@
+package com.francobm.magicosmetics.cache.inventories.menus;
+
+import com.francobm.magicosmetics.MagicCosmetics;
+import com.francobm.magicosmetics.api.CosmeticType;
+import com.francobm.magicosmetics.cache.Cosmetic;
+import com.francobm.magicosmetics.cache.PlayerCache;
+import com.francobm.magicosmetics.cache.Sound;
+import com.francobm.magicosmetics.cache.inventories.*;
+import com.francobm.magicosmetics.cache.items.Items;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SprayMenu extends PaginatedMenu {
+
+    public SprayMenu(String id, ContentMenu contentMenu, int startSlot, int endSlot, int backSlot, int nextSlot, int pagesSlot, List<Integer> slotsUnavailable) {
+        super(id, contentMenu, startSlot, endSlot, backSlot, nextSlot, pagesSlot, slotsUnavailable);
+    }
+
+    public SprayMenu(String id, ContentMenu contentMenu) {
+        super(id, contentMenu);
+    }
+
+    public SprayMenu(PlayerCache playerCache, Menu menu) {
+        super(playerCache, menu);
+    }
+
+    @Override
+    public void handleMenu(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        int slot = event.getSlot();
+        SlotMenu slotMenu = getContentMenu().getSlotMenuBySlot(slot);
+        if(slotMenu == null) return;
+        if(slotMenu.getItems().getId().endsWith("_spray")) {
+            if (slotMenu.getActionType().size() == 2) {
+                if (event.getClick() == ClickType.SHIFT_LEFT) {
+                    slotMenu.action(player, slotMenu.getActionType().get(1));
+                    setItems();
+                    return;
+                }
+                slotMenu.action(player, slotMenu.getActionType().get(0));
+                return;
+            }
+        }
+        if(slotMenu.getSlot() == getBackSlot()){
+            slotMenu.playSound(player);
+            if(page == 0){
+                //player.sendMessage(CustomCosmetics.getInstance().prefix + CustomCosmetics.getInstance().getMessages().getString("first-page"));
+                return;
+            }
+            page = page - 1;
+            open();
+            return;
+        }
+
+        if(slotMenu.getSlot() == getNextSlot()){
+            List<Cosmetic> cosmetics = Cosmetic.getCosmeticsUnHideByType(CosmeticType.SPRAY);
+            slotMenu.playSound(player);
+            if(((index + 1) >= cosmetics.size())){
+                //player.sendMessage(CustomCosmetics.getInstance().prefix + CustomCosmetics.getInstance().getMessages().getString("last-page"));
+                return;
+            }
+            page = page + 1;
+            open();
+            return;
+        }
+        slotMenu.action(player);
+    }
+
+    @Override
+    public void setItems() {
+        MagicCosmetics plugin = MagicCosmetics.getInstance();
+        getContentMenu().getSlots().resetSlots();
+        StringBuilder title = new StringBuilder();
+        title.append(getContentMenu().getTitle());
+        List<Cosmetic> cosmetics = Cosmetic.getCosmeticsUnHideByType(CosmeticType.SPRAY);
+        if(getBackSlot() != -1) {
+            SlotMenu s;
+            if(page == 0){
+                s = new SlotMenu(getBackSlot(), Items.getItem("back-button-cancel-template"), id, ActionType.OPEN_MENU);
+            }else{
+                s = new SlotMenu(getBackSlot(), Items.getItem("back-button-template"), id, ActionType.OPEN_MENU);
+            }
+            s.setSound(Sound.getSound("on_click_back_page"));
+            getContentMenu().addSlotMenu(s);
+        }
+        if(getPagesSlot() != -1) {
+            getContentMenu().addSlotMenu(new SlotMenu(getPagesSlot(), new Items(Items.getItem("pages-template").addVariableItem("%pages%", page + 1)), id, ActionType.CLOSE_MENU));
+        }
+        if(!cosmetics.isEmpty()) {
+            int a = 0;
+            for (int i = 0; i < getMaxItemsPerPage(); i++) {
+                index = getMaxItemsPerPage() * page + i;
+                if (index >= cosmetics.size()) break;
+                Cosmetic cosmetic = cosmetics.get(index);
+                int slot = (getStartSlot() + i + a);
+                if (cosmetic == null) continue;
+                while(slotsUnavailable.contains(slot)){
+                    slot++;
+                    a++;
+                }
+                title.append(getContentMenu().getSlots().isSlot(slot));
+                Items items = new Items(getPage()+index+"_spray", Items.getItem("spray-template").copyItem(playerCache, cosmetic, cosmetic.getItemStack()));
+                SlotMenu slotMenu;
+                items.addVariable("%equip%", playerCache.getEquip(cosmetic.getId()) != null ? MagicCosmetics.getInstance().getMessages().getString("equip") : MagicCosmetics.getInstance().getMessages().getString("unequip"));
+                if(plugin.isPermissions()){
+                    items.addVariable("%name%", cosmetic.getName()).addVariable("%available%", cosmetic.hasPermission(playerCache.getOfflinePlayer().getPlayer()) ? MagicCosmetics.getInstance().getMessages().getString("available") : MagicCosmetics.getInstance().getMessages().getString("unavailable")).addVariable("%type%", cosmetic.getCosmeticType());
+                    if(cosmetic.hasPermission(playerCache.getOfflinePlayer().getPlayer())){
+                        title.append(playerCache.getEquip(cosmetic.getId()) != null ? plugin.equip : plugin.ava);
+                    }else{
+                        title.append(plugin.unAva);
+                    }
+                }else {
+                    items.addVariable("%name%", cosmetic.getName()).addVariable("%available%", playerCache.getCosmeticById(cosmetic.getId()) != null ? MagicCosmetics.getInstance().getMessages().getString("available") : MagicCosmetics.getInstance().getMessages().getString("unavailable")).addVariable("%type%", cosmetic.getCosmeticType());
+                    if (playerCache.getCosmeticById(cosmetic.getId()) != null) {
+                        title.append(playerCache.getEquip(cosmetic.getId()) != null ? plugin.equip : plugin.ava);
+                    } else {
+                        title.append(plugin.unAva);
+                    }
+                }
+                title.append(getPanel(slot));
+                if(playerCache.getSpray() != null){
+                    if(playerCache.getSpray().getId().equalsIgnoreCase(cosmetic.getId())){
+                        slotMenu = new SlotMenu(slot, items, Collections.singletonList("magiccos unset " + cosmetic.getId()), ActionType.PLAYER_COMMAND);
+                    }else{
+                        if(cosmetic.isColored()){
+                            slotMenu = new SlotMenu(slot, items, Collections.singletonList("magiccos unuse " + cosmetic.getId()),"colored|color1|"+cosmetic.getId(), ActionType.OPEN_MENU, ActionType.PLAYER_COMMAND);
+                        }else{
+                            slotMenu = new SlotMenu(slot, items, cosmetic, ActionType.PREVIEW_ITEM, ActionType.PLAYER_COMMAND);
+                            slotMenu.getCommands().add("magiccos unuse " + cosmetic.getId());
+                        }
+                    }
+                }else{
+                    if(cosmetic.isColored()){
+                        slotMenu = new SlotMenu(slot, items, Collections.singletonList("magiccos unuse " + cosmetic.getId()),"colored|color1|"+cosmetic.getId(), ActionType.OPEN_MENU, ActionType.PLAYER_COMMAND);
+                    }else{
+                        slotMenu = new SlotMenu(slot, items, cosmetic, ActionType.PREVIEW_ITEM, ActionType.PLAYER_COMMAND);
+                        slotMenu.getCommands().add("magiccos unuse " + cosmetic.getId());
+                    }
+                }
+                slotMenu.setSound(Sound.getSound("on_click_cosmetic"));
+                getContentMenu().addSlotMenu(slotMenu);
+                setItemInPaginatedMenu(slotMenu, getPage(), index, "_spray");
+            }
+        }
+        if(getNextSlot() != -1){
+            SlotMenu s;
+            if(((index + 1) >= cosmetics.size())){
+                s = new SlotMenu(getNextSlot(), Items.getItem("next-button-cancel-template"), id, ActionType.OPEN_MENU);
+                //player.sendMessage(CustomCosmetics.getInstance().prefix + CustomCosmetics.getInstance().getMessages().getString("last-page"));
+            }else {
+                s = new SlotMenu(getNextSlot(), Items.getItem("next-button-template"), id, ActionType.OPEN_MENU);
+            }
+            s.setSound(Sound.getSound("on_click_next_page"));
+            getContentMenu().addSlotMenu(s);
+        }
+        for(SlotMenu slotMenu : getContentMenu().getSlotMenu().values()){
+            setItemInPaginatedMenu(slotMenu, -1, -1, "_spray");
+        }
+        MagicCosmetics.getInstance().getVersion().updateTitle(playerCache.getOfflinePlayer().getPlayer(), title.toString());
+    }
+
+}
