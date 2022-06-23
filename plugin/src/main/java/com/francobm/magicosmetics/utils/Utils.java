efee137f835e67519790a5c3434154b5d79e5dab
FrancoBM <baldeonjean@gmail.com>
2022-06-23 15:53:45 -0500

    feat: Improvements in useful and effective methods for other classes.

--- plugin/src/main/java/com/francobm/magicosmetics/utils/Utils.java
+++ plugin/src/main/java/com/francobm/magicosmetics/utils/Utils.java
@@ -1,20 +1,38 @@
 package com.francobm.magicosmetics.utils;
 
 import com.francobm.magicosmetics.MagicCosmetics;
+import com.francobm.magicosmetics.cache.Sound;
+import com.francobm.magicosmetics.cache.Spray;
 import com.francobm.magicosmetics.cache.User;
+import com.francobm.magicosmetics.cache.renderer.ImageRenderer;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+import org.bukkit.map.MapPalette;
+import org.bukkit.map.MapView;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +42,146 @@ public class Utils {
 
     public static String getVersion(){
         return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    }
+
+    public static String getTime(int time){
+        int hours = time / 3600;
+        int i = time - hours * 3600;
+        int minutes = i / 60;
+        int seconds = i - minutes * 60;
+        String secondsMsg;
+        String minutesMsg;
+        String hoursMsg;
+        if(seconds < 10) {
+            if(seconds == 1) {
+                secondsMsg = "0" + seconds + " second";
+            } else {
+                secondsMsg = "0" + seconds + " seconds";
+            }
+        } else {
+            secondsMsg = seconds + " seconds";
+        }
+        if(minutes < 10) {
+            if(minutes == 1) {
+                minutesMsg = "0" + minutes + " minute";
+            } else {
+                minutesMsg = "0" + minutes + " minutes";
+            }
+        } else {
+            minutesMsg = minutes + " minutes";
+        }
+        if(hours < 10) {
+            if(hours == 1) {
+                hoursMsg = "0" + hours + " hour";
+            } else {
+                hoursMsg = "0" + hours + " hours";
+            }
+        } else {
+            hoursMsg = hours + " hours";
+        }
+
+        if(hours != 0)
+        {
+            return hoursMsg + " " + minutesMsg + " " + secondsMsg;
+        }else if(minutes != 0) {
+            return minutesMsg + " " + secondsMsg;
+        }
+        return secondsMsg;
+    }
+
+    public static ItemStack getCustomHead(ItemStack itemStack, String texture){
+        if(itemStack == null) return null;
+        if(texture.isEmpty()){
+            return itemStack;
+        }
+        SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+        gameProfile.getProperties().put("textures", new Property("textures", texture));
+        try{
+            Field profileField = skullMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(skullMeta, gameProfile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        itemStack.setItemMeta(skullMeta);
+        return itemStack;
+    }
+
+    public static void sendSound(Player player, Sound sound) {
+        if(player == null) return;
+        if(sound == null) return;
+
+        if(sound.isCustom()) {
+            player.playSound(player.getLocation(), sound.getSoundCustom(), sound.getYaw(), sound.getPitch());
+            return;
+        }
+        player.playSound(player.getLocation(), sound.getSoundBukkit(), sound.getYaw(), sound.getPitch());
+    }
+
+    public static void sendAllSound(Location location, Sound sound) {
+        if(location.getWorld() == null) return;
+        if(sound == null) return;
+
+        if(sound.isCustom()) {
+            location.getWorld().playSound(location, sound.getSoundCustom(), sound.getYaw(), sound.getPitch());
+            return;
+        }
+        location.getWorld().playSound(location, sound.getSoundBukkit(), sound.getYaw(), sound.getPitch());
+    }
+
+    public static ItemStack getMapImage(Player player, BufferedImage image, Spray spray){
+        Color color = spray.getColor();
+        if(!spray.isPaint()) {
+            if (color != null) {
+                Graphics2D g = image.createGraphics();
+                g.setPaint(new java.awt.Color(color.getRed(), color.getGreen(), color.getBlue(), 120));
+                g.fillRect(0, 0, image.getWidth(), image.getHeight());
+                spray.setPaint(true);
+            }
+        }
+        MapView mapView = Bukkit.createMap(player.getWorld());
+        mapView.getRenderers().clear();
+        ImageRenderer imageRenderer = new ImageRenderer();
+        if(!imageRenderer.load(image)) return null;
+        mapView.addRenderer(imageRenderer);
+        ItemStack map = XMaterial.FILLED_MAP.parseItem();
+        MapMeta meta = (MapMeta) map.getItemMeta();
+        meta.setMapView(mapView);
+        map.setItemMeta(meta);
+        return map;
+    }
+
+    public static BufferedImage deepCopy(BufferedImage bi) {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPreMultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(bi.getRaster().createCompatibleWritableRaster());
+        return new BufferedImage(cm, raster, isAlphaPreMultiplied, null);
+    }
+
+    public static BufferedImage getImage(String url){
+        BufferedImage image;
+        try {
+            if(url.startsWith("http")) {
+                image = ImageIO.read(new URL(url));
+            }else{
+                File file = new File(MagicCosmetics.getInstance().getDataFolder(), "sprays/" + url);
+                if(!file.exists()) {
+                    return null;
+                }
+                image = ImageIO.read(file);
+            }
+            image = MapPalette.resizeImage(image);
+        } catch (IOException e) {
+            return null;
+        }
+        return image;
+    }
+
+    public static boolean isDyeable(ItemStack itemStack){
+        if(itemStack == null) return false;
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        return (itemMeta instanceof LeatherArmorMeta || itemMeta instanceof PotionMeta || itemMeta instanceof MapMeta);
     }
 
     public static void hidePlayer(Player player){
@@ -79,64 +237,13 @@ public class Utils {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
-    public static String bsc(String string){
-        return new String(Base64.getDecoder().decode(string));
+    public static int getRotation(float yaw, boolean allowDiagonals) {
+        if(allowDiagonals) return MathUtils.floor(((Location.normalizeYaw(yaw) + 180) * 8 / 360) + 0.5F) % 8;
+        return MathUtils.floor(((Location.normalizeYaw(yaw) + 180) * 4 / 360) + 0.5F) % 4;
     }
 
-    public static boolean siu(boolean debug) throws IOException {
-        MagicCosmetics.getInstance().getLogger().info(Utils.bsc("VmFsaWRhdGluZyBwdXJjaGFzZS4uLg=="));
-        String p = "%%__POLYMART__%%";
-        String m = "%%__MCMARKET__%%";
-        String s = "%%__SONGODA__%%";
-        String user_id = "%%__USER__%%";
-        String user_name = "%%__USERNAME__%%";
-        String inject_version = p.equalsIgnoreCase("1") ? "%%__INJECT_VER__%%" : "%%__VERSION__%%";
-        String resource_id = "%%__RESOURCE__%%";
-        String plugin_id = "%%__PLUGIN__%%";
-        String download_token = "%%__VERIFY_TOKEN__%%";
-        String nonce = "%%__NONCE__%%";
-        String download_agent = "%%__AGENT__%%";
-        String download_time = "%%__TIMESTAMP__%%";
-        if(p.equalsIgnoreCase("1")) {
-            MagicCosmetics.getInstance().setUser(new User(user_id, user_name, inject_version, resource_id, download_token, nonce, download_agent, download_time));
-            String api = "https://api.polymart.org/v1/verifyPurchase/";
-            URL url = new URL(api + "?inject_version=" + inject_version + "&resource_id=" + resource_id + "&user_id=" + user_id + "&nonce=" + nonce + "&download_agent=" + download_agent + "&download_time=" + download_time + "&download_token=" + download_token);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("accept", "application/json");
-
-            int responseCode = con.getResponseCode();
-            if (debug) {
-                MagicCosmetics.getInstance().getLogger().info("\nSending 'GET' request to URL : " + url);
-                MagicCosmetics.getInstance().getLogger().info("Response Code : " + responseCode);
-            }
-
-            boolean result;
-
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-
-                JsonObject json = new JsonParser().parse(response.toString()).getAsJsonObject();
-
-                result = json.get("response").getAsJsonObject().get("success").getAsBoolean();
-            }
-
-            return result;
-        }
-        if(m.equals("true")){
-            MagicCosmetics.getInstance().setUser(new User(user_id, user_name, inject_version, resource_id, download_token, nonce, download_agent, download_time));
-            return true;
-        }
-        if(s.equals("true")){
-            MagicCosmetics.getInstance().setUser(new User(user_id, user_name, inject_version, plugin_id, download_token, nonce, download_agent, download_time));
-            return true;
-        }
-        return false;
+    public static String bsc(String string){
+        return new String(Base64.getDecoder().decode(string));
     }
     /*public static String translateHexColorCodes(String startTag, String endTag, String message)
     {
