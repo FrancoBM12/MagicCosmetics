59af9e8e6c5742e59ec20e5a3a0feadd25ed7d43
FrancoBM <baldeonjean@gmail.com>
2022-07-03 03:58:32 -0500

    feat: New methods were added.

--- plugin/src/main/java/com/francobm/magicosmetics/provider/ItemsAdder.java
+++ plugin/src/main/java/com/francobm/magicosmetics/provider/ItemsAdder.java
@@ -1,10 +1,18 @@
 package com.francobm.magicosmetics.provider;
 
+import dev.lone.itemsadder.api.CustomEntity;
+import dev.lone.itemsadder.api.CustomPlayer;
 import dev.lone.itemsadder.api.CustomStack;
 import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class ItemsAdder {
+
+    public boolean existModel(String namespaceId) {
+        return CustomEntity.isInRegistry(namespaceId);
+    }
 
     public CustomStack getCustomStack(String id){
         return CustomStack.getInstance(id);
@@ -20,7 +28,23 @@ public class ItemsAdder {
         return customStack.getItemStack();
     }
 
+    public String replaceFontImageWithoutColor(String id){
+        return ChatColor.stripColor(FontImageWrapper.replaceFontImages(id));
+    }
+
     public String replaceFontImages(String id){
         return FontImageWrapper.replaceFontImages(id);
+    }
+
+    public void stopEmote(Player player){
+        CustomPlayer.stopEmote(player);
+    }
+
+    public boolean hasEmote(Player player){
+        try {
+            return CustomPlayer.byAlreadySpawned(player) != null;
+        } catch (Exception ignored) {
+        }
+        return false;
     }
 }
