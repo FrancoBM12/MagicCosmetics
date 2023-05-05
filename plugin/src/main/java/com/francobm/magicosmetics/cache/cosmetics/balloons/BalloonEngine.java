7b8bd6f810532911a0bff8267525ee493101100b
BuildTools <unconfigured@null.spigotmc.org>
2023-05-05 01:16:47 -0500

    feat: asynchronous methods were added for better balloon performance and the ModelEngine system was improved.

--- plugin/src/main/java/com/francobm/magicosmetics/cache/cosmetics/balloons/BalloonEngine.java
+++ plugin/src/main/java/com/francobm/magicosmetics/cache/cosmetics/balloons/BalloonEngine.java
@@ -3,33 +3,41 @@ package com.francobm.magicosmetics.cache.cosmetics.balloons;
 import com.francobm.magicosmetics.MagicCosmetics;
 import com.ticxo.modelengine.api.model.ActiveModel;
 import com.ticxo.modelengine.api.model.ModeledEntity;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.PufferFish;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public class BalloonEngine {
     private ModeledEntity modeledEntity;
+    private final List<UUID> players;
     private ActiveModel activeModel;
     private final String modelId;
     private final List<String> colorParts;
     private final String walk_animation;
     private final String idle_animation;
-    private LivingEntity entity;
+    private final double distance;
 
-    public BalloonEngine(String modelId, List<String> colorParts, String walk_animation, String idle_animation) {
+    public BalloonEngine(String modelId, List<String> colorParts, String walk_animation, String idle_animation, double distance) {
         this.modelId = modelId;
         this.colorParts = colorParts;
         this.walk_animation = walk_animation == null ? "walk" : walk_animation;
         this.idle_animation = idle_animation == null ? "idle" : idle_animation;
         this.modeledEntity = null;
         this.activeModel = null;
+        this.distance = distance;
+        players = new ArrayList<>();
     }
 
     public BalloonEngine getClone() {
-        return new BalloonEngine(modelId, new ArrayList<>(colorParts), walk_animation, idle_animation);
+        return new BalloonEngine(modelId, new ArrayList<>(colorParts), walk_animation, idle_animation, distance);
     }
 
     public void setState(int state){
@@ -57,18 +65,54 @@ public class BalloonEngine {
         }
     }
 
-    public void remove(){
+    public void remove(PufferFish leash){
         if(activeModel == null) return;
         if(modeledEntity == null) return;
         MagicCosmetics plugin = MagicCosmetics.getInstance();
+        for(Player player : modeledEntity.getRangeManager().getPlayerInRange()) {
+            removePlayer(leash, player);
+        }
         plugin.getModelEngine().removeModeledEntity(modeledEntity, activeModel);
         setActiveModel(null);
         setModeledEntity(null);
     }
 
-    public void detectPlayers() {
+    public void detectPlayers(PufferFish pufferFish, Entity owner) {
         if(modeledEntity == null) return;
-        MagicCosmetics.getInstance().getModelEngine().detectPlayers(modeledEntity);
+        for(Player player : Bukkit.getOnlinePlayers()){
+            if(players.contains(player.getUniqueId())) {
+                if(!owner.getWorld().equals(player.getWorld())) {
+                    removePlayer(pufferFish, player);
+                    continue;
+                }
+                if(owner.getLocation().distanceSquared(player.getLocation()) > distance) {
+                    removePlayer(pufferFish, player);
+                    continue;
+                }
+            }
+            if(!owner.getWorld().equals(player.getWorld())) continue;
+            if(owner.getLocation().distanceSquared(player.getLocation()) > distance) continue;
+            addPlayer(owner, pufferFish, player);
+        }
+        //MagicCosmetics.getInstance().getModelEngine().detectPlayers(modeledEntity, players);
+    }
+
+    private void addPlayer(Entity owner, PufferFish pufferFish, Player player) {
+        if(modeledEntity == null) return;
+        if(players.contains(player.getUniqueId())) return;
+        modeledEntity.getRangeManager().forceSpawn(player);
+        players.add(player.getUniqueId());
+        if(pufferFish == null) return;
+        MagicCosmetics.getInstance().getVersion().showFakePuffer(pufferFish, player);
+        MagicCosmetics.getInstance().getVersion().attachFakeEntity(owner, pufferFish, player);
+    }
+
+    public void removePlayer(PufferFish pufferFish, Player player) {
+        if(modeledEntity == null) return;
+        modeledEntity.getRangeManager().removePlayer(player);
+        players.remove(player.getUniqueId());
+        if(pufferFish == null) return;
+        MagicCosmetics.getInstance().getVersion().despawnFakeEntity(pufferFish, player);
     }
 
     public Set<String> getBones() {
@@ -76,7 +120,23 @@ public class BalloonEngine {
         return MagicCosmetics.getInstance().getModelEngine().getAllBonesIds(activeModel);
     }
 
+    public void spawnModel(Location location){
+        modeledEntity = MagicCosmetics.getInstance().getModelEngine().spawnModel(modelId, location);
+        activeModel = modeledEntity.getModel(modelId);
+    }
+
+    public void movementModel(Location location) {
+        if(modeledEntity == null) return;
+        MagicCosmetics.getInstance().getModelEngine().movementModel(modeledEntity, location);
+    }
+
+    public Location getMovementModel() {
+        if(modeledEntity == null) return null;
+        return modeledEntity.getBase().getLocation();
+    }
+
     public void tintModel(Color color) {
+        if(color == null) return;
         if(activeModel == null) return;
         MagicCosmetics plugin = MagicCosmetics.getInstance();
         for (String id : getBones()) {
@@ -111,11 +171,20 @@ public class BalloonEngine {
         return colorParts;
     }
 
-    public void setEntity(LivingEntity entity) {
-        this.entity = entity;
+    public PufferFish spawnLeash(Location location) {
+        return MagicCosmetics.getInstance().getVersion().spawnFakePuffer(location);
     }
 
-    public LivingEntity getEntity() {
-        return entity;
+    public void attachPufferFish(Player owner, Entity leashed) {
+        if(modeledEntity == null) return;
+        MagicCosmetics.getInstance().getVersion().attachFakeEntity(owner, leashed, modeledEntity.getRangeManager().getPlayerInRange().toArray(new Player[0]));
+    }
+
+    public void updatePositionLeash(Entity leashed, Location location) {
+        MagicCosmetics.getInstance().getVersion().updatePositionFakeEntity(leashed, location);
+    }
+
+    public void teleportLeash(Entity leashed) {
+        MagicCosmetics.getInstance().getVersion().teleportFakeEntity(leashed, modeledEntity.getRangeManager().getPlayerInRange().toArray(new Player[0]));
     }
 }
